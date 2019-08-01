de260448495a2c17b13a57f01bcc6a0e25d6e0b2
dacingorange <1930003850@qq.com>
2019-08-01 21:41:45 +0800

    'version2.0'

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
-console.log('test');
+window.onload=function(){
+    var oImport = document.querySelector('.import');
+    var oExport = document.querySelector('.export');
+
+    oImport.onkeyup=function(ev){
+        var oEvent = ev || event;
+        if(oEvent.keyCode == 13 && oEvent.ctrlKey){
+            oExport.innerHTML = oImport.value + oExport.innerHTML;
+            oImport.value = '';
+        }
+    };
+}
